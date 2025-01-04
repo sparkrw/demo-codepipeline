5958adaf41a565276d5b21ce06ff6e38ecf0fa54
sparkrw <107792237+sparkrw@users.noreply.github.com>
2025-01-04 11:09:19 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-console.log("hello, aws classroom!")
-const now = new Date();
-console.log(now.toString());
-console.log(now.toLocaleTimeString())
+console.log("hello, World")
+console.log("hello, AWS")
+console.log("hello, Lambda")
+
